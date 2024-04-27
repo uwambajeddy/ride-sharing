a01682cbc9bfa4320f6a8a4ef545eb403e991ab9
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-04-27 08:41:35 +0200

    main: initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: ''
-      }
-    ]
-  }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
